4caad96699203f67aaca99b023aa0974df93f55d
N30A <95051018+N30A@users.noreply.github.com>
2024-11-21 15:03:21 +0100

    Created address model

---
+++ api/Models/Address.cs
@@ -0,0 +1,11 @@
+﻿namespace api.Models
+{
+    public class Address
+    {
+        public string AddressID { get; set; }
+        public string Street { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
