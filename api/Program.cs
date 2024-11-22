41e31e60c871530dd47b7851ae2767b1540ef4be
N30A <95051018+N30A@users.noreply.github.com>
2024-11-22 18:00:32 +0100

    Created service methods, code move

--- api/Program.cs
+++ api/Program.cs
@@ -1,57 +1,31 @@
 using System.Data;
+using api.Repositories;
 using Microsoft.Data.SqlClient;
 
 namespace api
 {
     public class Program
     {
-        public static string BuildConnectionString(IConfiguration configuration, IWebHostEnvironment environment)
-        {   
-            string? server = configuration["DB_SERVER"];
-            ArgumentNullException.ThrowIfNullOrWhiteSpace(server, "Environment variable 'DB_SERVER' must be set");
-            string? database = configuration["DB_DATABASE"];
-            ArgumentNullException.ThrowIfNullOrWhiteSpace(database, "Environment variable 'DB_DATABASE' must be set");
-            string? user = configuration["DB_USER"];
-            ArgumentNullException.ThrowIfNullOrWhiteSpace(user, "Environment variable 'DB_USER' must be set");
-            string? password = configuration["DB_PASSWORD"];
-            ArgumentNullException.ThrowIfNullOrWhiteSpace(password, "Environment variable 'DB_PASSWORD' must be set");
-
-            string encrypt;
-            string trustCertificate;
-
-            if (environment.IsDevelopment())
-            {
-                encrypt = "False";
-                trustCertificate = "True";
-            }
-            else
-            {
-                encrypt = "True";
-                trustCertificate = "False";
-            }
-
-            return $"Server={server};Database={database};User Id={user};Password={password};Encrypt={encrypt};TrustServerCertificate={trustCertificate};";
-        }
-
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
             
             builder.Configuration.AddEnvironmentVariables();
 
-            string connectionString = BuildConnectionString(builder.Configuration, builder.Environment);
-
-            builder.Services.AddTransient<IDbConnection>(sc => new SqlConnection(connectionString));
+            builder.Services.AddDatabase(builder.Configuration, builder.Environment);
+            builder.Services.AddRepositories();
+            builder.Services.AddValidators();
 
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddRouting(options => options.LowercaseUrls = true);
 
             var app = builder.Build();
 
             if (app.Environment.IsDevelopment())
             {
-                app.UseSwagger();
+                app.UseSwagger(); 
                 app.UseSwaggerUI();
             }
             else
---
+++ api/ServiceExtensions.cs
@@ -0,0 +1,60 @@
+﻿using System.Data;
+using api.Dto.Validators;
+using api.Repositories;
+using Microsoft.Data.SqlClient;
+
+namespace api
+{
+    public static class ServiceExtensions
+    {
+        private static string GetEnvironmentVariablesOrThrow(IConfiguration configuration, string key)
+        {
+            var value = configuration[key];
+
+            if (string.IsNullOrWhiteSpace(value))
+            {   
+                throw new InvalidOperationException($"Environment variable '{key}' is required but was not set.");
+            }
+
+            return value;
+        }
+
+        private static string BuildConnectionString(IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            string server = GetEnvironmentVariablesOrThrow(configuration, "DB_SERVER");
+            string database = GetEnvironmentVariablesOrThrow(configuration, "DB_DATABASE");
+            string user = GetEnvironmentVariablesOrThrow(configuration, "DB_USER");
+            string password = GetEnvironmentVariablesOrThrow(configuration, "DB_PASSWORD");
+
+            string encrypt = environment.IsDevelopment() ? "False" : "True";
+            string trustCertificate = environment.IsDevelopment() ? "True" : "False";
+
+            return $"Server={server};Database={database};User Id={user};Password={password};Encrypt={encrypt};TrustServerCertificate={trustCertificate};";
+        }
+
+        public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            string connectionString = BuildConnectionString(configuration, environment);
+
+            services.AddTransient<IDbConnection>(sc => new SqlConnection(connectionString));
+
+            return services;
+        }
+
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<AddressRepository>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddValidators(this IServiceCollection services)
+        {
+            services.AddScoped<AddressCreateValidator>();
+            services.AddScoped<AddressUpdateValidator>();
+
+            return services;
+        }
+    }
+}
+
