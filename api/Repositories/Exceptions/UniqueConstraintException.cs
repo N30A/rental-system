5c70090c2c10f1db248985cf515684b75da91882
N30A <95051018+N30A@users.noreply.github.com>
2024-11-22 17:58:23 +0100

    Created UniqueConstraintException

---
+++ api/Repositories/Exceptions/UniqueConstraintException.cs
@@ -0,0 +1,9 @@
+﻿namespace api.Repositories.Exceptions
+{
+    public class UniqueConstraintException : Exception
+    {
+        public static readonly int Number = 2627;
+
+        public UniqueConstraintException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
