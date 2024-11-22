24347dd465617e148aae72819db5f297a8478a99
N30A <95051018+N30A@users.noreply.github.com>
2024-11-22 17:57:11 +0100

    Basic DTO validation

---
+++ api/Dto/Validators/AddressCreateValidator.cs
@@ -0,0 +1,16 @@
+﻿using api.Dto;
+using FluentValidation;
+
+namespace api.Dto.Validators
+{
+    public class AddressCreateValidator : AbstractValidator<AddressCreate>
+    {
+        public AddressCreateValidator()
+        {
+            RuleFor(address => address.Street).NotEmpty();
+            RuleFor(address => address.PostalCode).NotEmpty();
+            RuleFor(address => address.City).NotEmpty();
+            RuleFor(address => address.Country).NotEmpty();
+        }
+    }
+}
---
+++ api/Dto/Validators/AddressUpdateValidator.cs
@@ -0,0 +1,16 @@
+﻿using api.Dto;
+using FluentValidation;
+
+namespace api.Dto.Validators
+{
+    public class AddressUpdateValidator : AbstractValidator<AddressUpdate>
+    {
+        public AddressUpdateValidator()
+        {
+            RuleFor(address => address.Street).NotEmpty();
+            RuleFor(address => address.PostalCode).NotEmpty();
+            RuleFor(address => address.City).NotEmpty();
+            RuleFor(address => address.Country).NotEmpty();
+        }
+    }
+}
