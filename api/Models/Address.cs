424c0749de1d218a330581df2ac01d702c9bec83
N30A <95051018+N30A@users.noreply.github.com>
2024-11-22 17:54:49 +0100

    Changed AddressID type

--- api/Models/Address.cs
+++ api/Models/Address.cs
@@ -2,10 +2,12 @@
 {
     public class Address
     {
-        public string AddressID { get; set; }
+        public int AddressID { get; set; }
         public string Street { get; set; }
         public string PostalCode { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
+
+        public Address() { }
     }
 }
