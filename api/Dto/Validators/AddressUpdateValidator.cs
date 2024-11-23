44498214b46d4c0a9673822e16b90a69ec017652
N30A <95051018+N30A@users.noreply.github.com>
2024-11-23 19:04:34 +0100

    Validation messages

--- api/Dto/Validators/AddressUpdateValidator.cs
+++ api/Dto/Validators/AddressUpdateValidator.cs
@@ -7,10 +7,21 @@ namespace api.Dto.Validators
     {
         public AddressUpdateValidator()
         {
-            RuleFor(address => address.Street).NotEmpty();
-            RuleFor(address => address.PostalCode).NotEmpty();
-            RuleFor(address => address.City).NotEmpty();
-            RuleFor(address => address.Country).NotEmpty();
+            RuleFor(address => address.Street)
+                .NotEmpty()
+                .WithMessage("Street is required.");
+
+            RuleFor(address => address.PostalCode)
+                .NotEmpty()
+                .WithMessage("PostalCode is required.");
+
+            RuleFor(address => address.City)
+                .NotEmpty()
+                .WithMessage("City is required.");
+
+            RuleFor(address => address.Country)
+                .NotEmpty()
+                .WithMessage("Country is required.");
         }
     }
 }
