f0cb021244f21ee40e5278f6bd7f2a9d8a97f747
N30A <95051018+N30A@users.noreply.github.com>
2024-11-24 22:25:37 +0100

    More SQL error numbers

--- api/Repositories/SqlErrorNumbers.cs
+++ api/Repositories/SqlErrorNumbers.cs
@@ -2,6 +2,10 @@
 {
     public enum SqlErrorNumbers
     {
-        UniqueConstraintViolation = 2627
+        UniqueConstraintViolation = 2627,       // Violation of a UNIQUE constraint.
+        ForeignKeyConstraintViolation = 547,    // Attempt to insert or update data that breaks a FOREIGN KEY relationship.
+        CheckConstraintViolation = 547,         // Violation of a CHECK constraint.
+        Timeout = -2,                           // The database took too long to respond or complete the operation.
+        Deadlock = 1205                         // One or more transactions were terminated due to a deadlock.
     }
 }
