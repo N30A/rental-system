03550059a4749e1c942e6da3c1e2ece9a05be3e0
N30A <95051018+N30A@users.noreply.github.com>
2024-11-24 14:35:18 +0100

    Added interfaces for Address service and repository

---
+++ api/Repositories/IAddressRepository.cs
@@ -0,0 +1,19 @@
+﻿using api.Models;
+using api.Models.Domain;
+using api.Models.Dto;
+
+namespace api.Repositories
+{
+    public interface IAddressRepository
+    {
+        Task<Result<Address>> CreateAddressAsync(CreateAddressRequest request);
+
+        Task<Result<Address>> GetAddressByIDAsync(int id);
+
+        Task<Result<IEnumerable<Address>>> GetAddressesAsync(string sortColumn, string sortOrder);
+
+        Task<Result<Address>> UpdateAddressByIDAsync(int id, UpdateAddressRequest request);
+
+        Task<Result<Address>> DeleteAddressAsync(int id);
+    }
+}
---
+++ api/Services/IAddressService.cs
@@ -0,0 +1,19 @@
+﻿using api.Models;
+using api.Models.Domain;
+using api.Models.Dto;
+
+namespace api.Services
+{
+    public interface IAddressService
+    {
+        Task<Result<AddressResponse>> CreateAddressAsync(CreateAddressRequest request);
+
+        Task<Result<AddressResponse>> GetAddressByIDAsync(int id);
+
+        Task<Result<IEnumerable<AddressResponse>>> GetAddressesAsync(string sortColumn, string sortOrder);
+
+        Task<Result<AddressResponse>> UpdateAddressByIDAsync(int id, UpdateAddressRequest request);
+
+        Task<Result<Address>> DeleteAddressAsync(int id);
+    }
+}
