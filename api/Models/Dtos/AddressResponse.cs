e3756c68d66f20221a0757de33607508af22e7c2
N30A <95051018+N30A@users.noreply.github.com>
2024-11-24 14:35:38 +0100

    Added AddressResponse dto

---
+++ api/Models/Dtos/AddressResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace api.Models.Dto
+{
+    public class AddressResponse
+    {
+        public string Street { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+        public AddressResponse() { }
+    }
+}
