9aafafb35d858ef6fe0d370389726e5980d9251a
N30A <95051018+N30A@users.noreply.github.com>
2024-11-24 15:30:07 +0100

    New multiple address response dto

---
+++ api/Models/Dtos/MultipleAddressesResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace api.Models.Dtos
+{
+    public class MultipleAddressesResponse
+    {   
+        public IEnumerable<AddressResponse> Data { get; set; } = new List<AddressResponse>();
+        public int Count => Data.Count();
+        
+        public MultipleAddressesResponse() { }
+    }
+}
--- api/Services/Interfaces/IAddressService.cs
+++ api/Services/Interfaces/IAddressService.cs
@@ -9,7 +9,7 @@ namespace api.Services.Interfaces
 
         Task<Result<AddressResponse>> GetAddressByIDAsync(int id);
 
-        Task<Result<IEnumerable<AddressResponse>>> GetAddressesAsync(string sortColumn, string sortOrder);
+        Task<Result<MultipleAddressesResponse>> GetAddressesAsync(string sortColumn, string sortOrder);
 
         Task<Result<AddressResponse>> UpdateAddressByIDAsync(int id, UpdateAddressRequest request);
 
