e9152094df734e771eb5589e3da2e33c16787026
N30A <95051018+N30A@users.noreply.github.com>
2024-11-24 16:37:05 +0100

    Added service for addresses

---
+++ api/Services/AddressService.cs
@@ -0,0 +1,45 @@
+﻿using api.Models;
+using api.Models.Domain;
+using api.Models.Dtos;
+using api.Repositories.Interfaces;
+using api.Services.Interfaces;
+
+namespace api.Services
+{
+    public class AddressService : IAddressService
+    {
+        private readonly ILogger<AddressService> _logger;
+        private readonly IAddressRepository _repository;
+
+        public AddressService(ILogger<AddressService> logger, IAddressRepository repository)
+        {
+            _logger = logger;
+            _repository = repository;
+        }
+
+        public async Task<Result<AddressResponse>> CreateAddressAsync(CreateAddressRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Result<AddressResponse>> GetAddressByIDAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Result<MultipleAddressesResponse>> GetAddressesAsync(string sortColumn, string sortOrder)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Result<AddressResponse>> UpdateAddressByIDAsync(int id, UpdateAddressRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Result<AddressResponse>> DeleteAddressAsync(int id)
+        {
+            throw new NotImplementedException();
+        } 
+    }
+}
