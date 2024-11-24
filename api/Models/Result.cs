25cf9c329d2bcb0c16c0db24784d3cacb6b1d870
N30A <95051018+N30A@users.noreply.github.com>
2024-11-24 14:34:24 +0100

    Added Result<T> class for handling operation results

---
+++ api/Models/Result.cs
@@ -0,0 +1,31 @@
+﻿namespace api.Models
+{
+    public class Result<T>
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public T? Data { get; set; }
+
+        private Result(bool success, string message, T? data)
+        {
+            Success = success;
+            Message = message;
+            Data = data;
+        }
+
+        public static Result<T> SuccessResult(T? data = default)
+        {
+            return SuccessResult(string.Empty, data);
+        }
+
+        public static Result<T> SuccessResult(string message, T? data = default)
+        {
+            return new Result<T>(true, message, data);
+        }
+
+        public static Result<T> FailureResult(string message, T? data = default)
+        {
+            return new Result<T>(false, message, data);
+        }
+    }
+}
